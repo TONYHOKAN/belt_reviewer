df7efff1cc57d1aa0b8d3db580fc04303fe41adc
Tony Ng <tony.hokan@gmail.com>
2017-04-18 23:12:38 +0800

    create events_controller#create and related template for adding new event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,26 @@
 class EventsController < ApplicationController
   def index
     @events = Event.all
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if !@event.save
+      error_message
+    end
+    redirect_to index
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:name, :date, :location)
+  end
+
+  def error_message
+    flash[:event] = @event
+    flash[:error] = true
+    flash[:error_count] = @event.errors.count
+    flash[:full_error_messages] = @event.errors.full_messages
   end
 end
