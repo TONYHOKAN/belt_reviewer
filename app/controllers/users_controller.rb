8600ba8b799b66ff8c52b7dbde4936f02238a726
Tony Ng <tony.hokan@gmail.com>
2017-04-18 22:05:12 +0800

    create users_controller#update to allow user update their profile add error handling when update fail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,17 +13,29 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to @user, notice: "Thanks"
     else
-      flash[:user] = @user
-      flash[:error] = true
-      flash[:error_count] = @user.errors.count
-      flash[:full_error_messages] = @user.errors.full_messages
+      error_message
       redirect_to url_for(:controller => :home_page, :action => :index)
     end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if !@user.update(user_params)
+      error_message
+    end
+    redirect_to @user
   end
 
   private
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+  def error_message
+    flash[:user] = @user
+    flash[:error] = true
+    flash[:error_count] = @user.errors.count
+    flash[:full_error_messages] = @user.errors.full_messages
   end
 end
