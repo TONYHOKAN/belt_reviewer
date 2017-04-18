c639aff414366742961fc1ccc0e1fda19f31befa
Tony Ng <tony.hokan@gmail.com>
2017-04-18 13:42:27 +0800

    create user model and related db

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true,
+      format: /\A\S+@\S+\z/,
+      uniqueness: {case_sensitive: false}
+
+end
