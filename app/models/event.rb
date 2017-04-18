3995fbc370d6dd357dfa4760cc276420de86d258
Tony Ng <tony.hokan@gmail.com>
2017-04-18 23:49:43 +0800

    add validation for event creation that date must in future

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,13 @@ class Event < ApplicationRecord
   validates :name, presence: true
   validates :date, presence: true
   validates :location, presence: true
+
+  validate :validate_future_date_event
+
+
+  def validate_future_date_event
+    if date.present? && date < Date.today
+      errors.add(:date, "Event date must be in future")
+    end
+  end
 end
