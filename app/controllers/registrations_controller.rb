78c71e4e48cf6e135411fcf1bf94f74066bca593
Tony Ng <tony.hokan@gmail.com>
2017-04-19 04:20:37 +0800

    create registrations_controller#new to allow user to join event create registrations_controller#destroy to allow user cancel joining event update events_controller#index template to show join and cancel joining event text

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,12 @@
 class RegistrationsController < ApplicationController
+  def new
+    Registration.new(:user_id => session[:user_id], :event_id => params[:event_id]).save
+    redirect_to url_for(:controller => :events, :action => :index)
+  end
+
+  def destroy
+    @registration = Registration.find(params[:id])
+    @registration.destroy
+    redirect_to url_for(:controller => :events, :action => :index)
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
       User.find(session[:user_id])
     end
   end
+
+  def is_user_joined_event?(user_id, event_id)
+    Registration.find_by(user_id:user_id, event_id:event_id)
+  end
 end
